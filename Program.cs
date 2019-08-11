d28e18cba9c42640ede35d78e4cea613090ab21a
Sechere <jermains@computercastlesltd.co.ke>
2019-08-11 12:14:34 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ASCIIconverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a word : ");
+            void Emptyline()
+            {
+                Console.WriteLine("");
+            }
+            string input = Console.ReadLine();
+
+            Console.WriteLine("Your Input : {0}", input);
+            Emptyline();
+            Console.WriteLine("Output : ");
+            foreach(char c in input)
+            {
+                Console.WriteLine(System.Convert.ToInt32(c));
+            }
+        }
+    }
+}
